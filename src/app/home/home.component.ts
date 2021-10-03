07bb2e85005a41af538e5824e7b80cefb22de334
Hitesh Mandav <mandavhitesh@gmail.com>
2021-10-04 00:46:51 +0530

    Added code for socket io message send and message recive

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, AfterViewInit, ElementRef, ViewChild } from '@angular/core';
-import { MessageType } from './types'
+import { MessageType } from './types';
+import * as client from 'socket.io-client';
 
 @Component({
   selector: 'app-home',
@@ -13,19 +14,36 @@ export class HomeComponent implements OnInit, AfterViewInit {
 
   public message = '';
   public messageList: MessageType[];
+  private socket: any;
+  public canStartChat: boolean;
 
 
   constructor() { }
 
   ngOnInit(): void {
     this.messageList = [];
+    this.socket = client.io(`localhost:3000`);
+    this.socket.on('connect', () => {
+      this.displayMessage(`You are connected with Id : ${this.socket.id}`, 'other', false);
+      console.log(`socket :: $`)
+    });
+
+    this.socket.on('recive-message', (msg) => {
+      console.log(`message recived ${msg}`)
+      this.displayMessage(msg, 'other', false)
+    });
+  }
+
+  startChat(){
+
   }
 
   sendMessage(){
     console.log(`send Message ${this.message}`);
-    
+
     if(this.message !== '') {
-      this.displayMessage(this.message, 'You', true); 
+      this.displayMessage(this.message, 'You', true);
+      this.socket.emit('send-message', this.message);
       this.message = '';
     } else {
 
